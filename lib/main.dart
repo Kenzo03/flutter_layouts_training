087c96591a7cc953c74809f8656c1058a10d0f00
kenzo03 <kevinwijaya480@gmail.com>
2022-02-28 18:26:32 +0700

    feature: add title and button section

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(children: [
+          Expanded(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              Text('Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]))
+            ]),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ]));
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
         title: 'Flutter Layout Demo',
         theme: ThemeData(
@@ -17,6 +48,20 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
             appBar: AppBar(title: const Text('Flutter layout demo')),
-            body: const Center(child: Text('Hello World'))));
+            body: Column(children: [titleSection, buttonSection])));
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 12, fontWeight: FontWeight.w400, color: color)))
+        ]);
   }
 }
